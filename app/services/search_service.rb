12e3c093b9ba678b0dea13603e917bb2a4e36182
nAzgalor <nazgalor@gmail.com>
2017-05-19 17:21:54 +0300

    improved view

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -1,13 +1,12 @@
 class SearchService
   def initialize(params)
     @start_place = params[:start_place]
-    @start_at = params[:start_at].to_date
+    @start_at = params[:start_at].to_date if params[:start_at].present?
   end
 
   def results
     buses = Bus.all
     buses = buses.where(start_place: @start_place) if @start_place.present?
-    # binding.pry
     buses = buses.where(start_at: @start_at.beginning_of_day..@start_at.end_of_day) if @start_at.present?
     buses
   end
